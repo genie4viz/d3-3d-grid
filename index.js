e38d6b27787a07b51d7c9f16090147930ebcfeef
selistym <selistym@protonmail.com>
2019-01-05 09:22:29 -0800

    scatter plot graph completed

--- index.js
+++ index.js
@@ -1,106 +1,135 @@
-var origin = [480, 300], scale = 20, j = 10, cubesData = [], alpha = 0, beta = 0, startAngle = Math.PI / 6;
+var origin = [480, 300], j = 10, scale = 20, scatter = [], yLine = [], xGrid = [], beta = 0, alpha = 0, key = function (d) { return d.id; }, startAngle = Math.PI / 4;
 var svg = d3.select('svg').call(d3.drag().on('drag', dragged).on('start', dragStart).on('end', dragEnd)).append('g');
 var color = d3.scaleOrdinal(d3.schemeCategory20);
-var cubesGroup = svg.append('g').attr('class', 'cubes');
 var mx, my, mouseX, mouseY;
 
-var cubes3D = d3._3d()
-    .shape('CUBE')
+var grid3d = d3._3d()
+    .shape('GRID', 20)
+    .origin(origin)
+    .rotateY(startAngle)
+    .rotateX(-startAngle)
+    .scale(scale);
+
+var point3d = d3._3d()
     .x(function (d) { return d.x; })
     .y(function (d) { return d.y; })
     .z(function (d) { return d.z; })
+    .origin(origin)
     .rotateY(startAngle)
     .rotateX(-startAngle)
+    .scale(scale);
+
+var yScale3d = d3._3d()
+    .shape('LINE_STRIP')
     .origin(origin)
+    .rotateY(startAngle)
+    .rotateX(-startAngle)
     .scale(scale);
 
 function processData(data, tt) {
 
-    /* --------- CUBES ---------*/
+    /* ----------- GRID ----------- */
 
-    var cubes = cubesGroup.selectAll('g.cube').data(data, function (d) { return d.id });
+    var xGrid = svg.selectAll('path.grid').data(data[0], key);
 
-    var ce = cubes
+    xGrid
         .enter()
-        .append('g')
-        .attr('class', 'cube')
-        .attr('fill', function (d) { return color(d.id); })
-        .attr('stroke', function (d) { return d3.color(color(d.id)).darker(2); })
-        .merge(cubes)
-        .sort(cubes3D.sort);
-
-    cubes.exit().remove();
-
-    /* --------- FACES ---------*/
-
-    var faces = cubes.merge(ce).selectAll('path.face').data(function (d) { return d.faces; }, function (d) { return d.face; });
-
-    faces.enter()
         .append('path')
-        .attr('class', 'face')
-        .attr('fill-opacity', 0.95)
-        .classed('_3d', true)
-        .merge(faces)
+        .attr('class', '_3d grid')
+        .merge(xGrid)
+        .attr('stroke', 'black')
+        .attr('stroke-width', 0.3)
+        .attr('fill', function (d) { return d.ccw ? 'lightgrey' : '#717171'; })
+        .attr('fill-opacity', 0.9)
+        .attr('d', grid3d.draw);
+
+    xGrid.exit().remove();
+
+    /* ----------- POINTS ----------- */
+
+    var points = svg.selectAll('circle').data(data[1], key);
+
+    points
+        .enter()
+        .append('circle')
+        .attr('class', '_3d')
+        .attr('opacity', 0)
+        .attr('cx', posPointX)
+        .attr('cy', posPointY)
+        .merge(points)
         .transition().duration(tt)
-        .attr('d', cubes3D.draw);
+        .attr('r', 3)
+        .attr('stroke', function (d) { return d3.color(color(d.id)).darker(3); })
+        .attr('fill', function (d) { return color(d.id); })
+        .attr('opacity', 1)
+        .attr('cx', posPointX)
+        .attr('cy', posPointY);
 
-    faces.exit().remove();
+    points.exit().remove();
 
-    /* --------- TEXT ---------*/
+    /* ----------- y-Scale ----------- */
 
-    var texts = cubes.merge(ce).selectAll('text.text').data(function (d) {
-        var _t = d.faces.filter(function (d) {
-            return d.face === 'top';
-        });
-        return [{ height: d.height, centroid: _t[0].centroid }];
-    });
+    var yScale = svg.selectAll('path.yScale').data(data[2]);
 
-    texts
+    yScale
+        .enter()
+        .append('path')
+        .attr('class', '_3d yScale')
+        .merge(yScale)
+        .attr('stroke', 'black')
+        .attr('stroke-width', .5)
+        .attr('d', yScale3d.draw);
+
+    yScale.exit().remove();
+
+    /* ----------- y-Scale Text ----------- */
+
+    var yText = svg.selectAll('text.yText').data(data[2][0]);
+
+    yText
         .enter()
         .append('text')
-        .attr('class', 'text')
-        .attr('dy', '-.7em')
-        .attr('text-anchor', 'middle')
-        .attr('font-family', 'sans-serif')
-        .attr('font-weight', 'bolder')
-        .attr('x', function (d) { return origin[0] + scale * d.centroid.x })
-        .attr('y', function (d) { return origin[1] + scale * d.centroid.y })
-        .classed('_3d', true)
-        .merge(texts)
-        .transition().duration(tt)
-        .attr('fill', 'black')
-        .attr('stroke', 'none')
-        .attr('x', function (d) { return origin[0] + scale * d.centroid.x })
-        .attr('y', function (d) { return origin[1] + scale * d.centroid.y })
-        .tween('text', function (d) {
-            var that = d3.select(this);
-            var i = d3.interpolateNumber(+that.text(), Math.abs(d.height));
-            return function (t) {
-                that.text(i(t).toFixed(1));
-            };
-        });
+        .attr('class', '_3d yText')
+        .attr('dx', '.3em')
+        .merge(yText)
+        .each(function (d) {
+            d.centroid = { x: d.rotated.x, y: d.rotated.y, z: d.rotated.z };
+        })
+        .attr('x', function (d) { return d.projected.x; })
+        .attr('y', function (d) { return d.projected.y; })
+        .text(function (d) { return d[1] <= 0 ? d[1] : ''; });
 
-    texts.exit().remove();
+    yText.exit().remove();
 
-    /* --------- SORT TEXT & FACES ---------*/
+    d3.selectAll('._3d').sort(d3._3d().sort);
+}
 
-    ce.selectAll('._3d').sort(d3._3d().sort);
+function posPointX(d) {
+    return d.projected.x;
+}
 
+function posPointY(d) {
+    return d.projected.y;
 }
 
 function init() {
-    cubesData = [];
     var cnt = 0;
-    for (var z = -j / 2; z <= j / 2; z = z + 5) {
-        for (var x = -j; x <= j; x = x + 5) {
-            var h = d3.randomUniform(-2, -7)();
-            var _cube = makeCube(h, x, z);
-            _cube.id = 'cube_' + cnt++;
-            _cube.height = h;
-            cubesData.push(_cube);
+    xGrid = [], scatter = [], yLine = [];
+    for (var z = -j; z < j; z++) {
+        for (var x = -j; x < j; x++) {
+            xGrid.push([x, 1, z]);
+            scatter.push({ x: x, y: d3.randomUniform(0, -10)(), z: z, id: 'point_' + cnt++ });
         }
     }
-    processData(cubes3D(cubesData), 1000);
+
+    d3.range(-1, 11, 1).forEach(function (d) { yLine.push([-j, -d, -j]); });
+
+    var data = [
+        grid3d(xGrid),
+        point3d(scatter),
+        yScale3d([yLine])
+    ];
+    processData(data, 1000);
 }
 
 function dragStart() {
@@ -113,25 +142,17 @@ function dragged() {
     mouseY = mouseY || 0;
     beta = (d3.event.x - mx + mouseX) * Math.PI / 230;
     alpha = (d3.event.y - my + mouseY) * Math.PI / 230 * (-1);
-    processData(cubes3D.rotateY(beta + startAngle).rotateX(alpha - startAngle)(cubesData), 0);
+    var data = [
+        grid3d.rotateY(beta + startAngle).rotateX(alpha - startAngle)(xGrid),
+        point3d.rotateY(beta + startAngle).rotateX(alpha - startAngle)(scatter),
+        yScale3d.rotateY(beta + startAngle).rotateX(alpha - startAngle)([yLine]),
+    ];
+    processData(data, 0);
 }
 
 function dragEnd() {
     mouseX = d3.event.x - mx + mouseX;
     mouseY = d3.event.y - my + mouseY;
-}
-
-function makeCube(h, x, z) {
-    return [
-        { x: x - 1, y: h, z: z + 1 }, // FRONT TOP LEFT
-        { x: x - 1, y: 0, z: z + 1 }, // FRONT BOTTOM LEFT
-        { x: x + 1, y: 0, z: z + 1 }, // FRONT BOTTOM RIGHT
-        { x: x + 1, y: h, z: z + 1 }, // FRONT TOP RIGHT
-        { x: x - 1, y: h, z: z - 1 }, // BACK  TOP LEFT
-        { x: x - 1, y: 0, z: z - 1 }, // BACK  BOTTOM LEFT
-        { x: x + 1, y: 0, z: z - 1 }, // BACK  BOTTOM RIGHT
-        { x: x + 1, y: h, z: z - 1 }, // BACK  TOP RIGHT
-    ];
 }
 
 d3.selectAll('button').on('click', init);
