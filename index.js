56def6a538126f7eb6d816533f0ca0cbd825c870
selistym <selistym@protonmail.com>
2019-01-07 14:41:01 -0800

    one cube can draggable on grid

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var origin = [700, 450], grid_size = 20, scale = 20, xGrid = [], beta = 0, alpha = 0, key = function (d) { return d.id; }, startAngle = Math.PI / 8;
-var svg = d3.select('svg').call(d3.drag().on('drag', dragged).on('start', dragStart).on('end', dragEnd)).append('g');
+//var svg = d3.select('svg').call(d3.drag().on('drag', dragged).on('start', dragStart).on('end', dragEnd)).append('g');
+var svg = d3.select('svg').append('g');
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 var mx, my, mouseX, mouseY;
 
@@ -50,13 +51,13 @@ function processCubes(data, tt) {
         .enter()
         .append('g')
         .attr('class', 'cube')
+        .call(d3.drag().on('drag', draggedCube).on('start', dragStartCube).on('end', dragEndCube))
         .attr('fill', function (d) { return color(d.id); })
         .attr('stroke', function (d) { return d3.color(color(d.id)).darker(2); })
         .merge(cubes)
         .sort(cubes3D.sort);
 
     cubes.exit().remove();
-
     /* --------- FACES ---------*/
 
     var faces = cubes.merge(ce).selectAll('path.face').data(function (d) { return d.faces; }, function (d) { return d.face; });
@@ -71,7 +72,7 @@ function processCubes(data, tt) {
         .attr('d', cubes3D.draw);
 
     faces.exit().remove();
-   
+
     /* --------- SORT TEXT & FACES ---------*/
 
     ce.selectAll('._3d').sort(d3._3d().sort);
@@ -98,33 +99,34 @@ function init() {
             cubesData.push(_cube);
             break;
         }
+        break;
     }
-    
+
     processGrid(data);
     processCubes(cubes3D(cubesData), 1000);
 }
 
-function dragStart() {
-    mx = d3.event.x;
-    my = d3.event.y;
-}
+// function dragStart() {
+//     mx = d3.event.x;
+//     my = d3.event.y;
+// }
 
-function dragged() {
-    mouseX = mouseX || 0;
-    mouseY = mouseY || 0;
-    beta = (d3.event.x - mx + mouseX) * Math.PI / 230;
-    alpha = (d3.event.y - my + mouseY) * Math.PI / 230 * (-1);
-    var data = grid3d.rotateY(beta + startAngle).rotateX(alpha - startAngle)(xGrid);
-    processGrid(data, 0);
-    processCubes(cubes3D.rotateY(beta + startAngle).rotateX(alpha - startAngle)(cubesData), 0);
-    
+// function dragged() {
+//     mouseX = mouseX || 0;
+//     mouseY = mouseY || 0;
+//     beta = (d3.event.x - mx + mouseX) * Math.PI / 230;
+//     alpha = (d3.event.y - my + mouseY) * Math.PI / 230 * (-1);
+//     var data = grid3d.rotateY(beta + startAngle).rotateX(alpha - startAngle)(xGrid);
+//     processGrid(data, 0);
+//     processCubes(cubes3D.rotateY(beta + startAngle).rotateX(alpha - startAngle)(cubesData), 0);
 
-}
 
-function dragEnd() {
-    mouseX = d3.event.x - mx + mouseX;
-    mouseY = d3.event.y - my + mouseY;
-}
+// }
+
+// function dragEnd() {
+//     mouseX = d3.event.x - mx + mouseX;
+//     mouseY = d3.event.y - my + mouseY;
+// }
 function makeCube(h, x, z) {
     return [
         { x: x - 1, y: h, z: z + 1 }, // FRONT TOP LEFT
@@ -136,6 +138,22 @@ function makeCube(h, x, z) {
         { x: x + 1, y: 0, z: z - 1 }, // BACK  BOTTOM RIGHT
         { x: x + 1, y: h, z: z - 1 }, // BACK  TOP RIGHT
     ];
+}
+
+//drag handler for cube
+function draggedCube(d) {
+
+    mouseX = mouseX || 0;
+    mouseY = mouseY || 0;    
+    processCubes(cubes3D.origin([d3.event.x + 450 , d3.event.y])(cubesData), 0);
+}
+function dragStartCube() {
+    mx = d3.event.x;
+    my = d3.event.y;    
+}
+function dragEndCube() {
+    mouseX = d3.event.x - mx + mouseX;
+    mouseY = d3.event.y - my + mouseY;
 }
 d3.selectAll('button').on('click', init);
 
